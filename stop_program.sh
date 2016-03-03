056d7e2a3b48b6a7febfde7b829270efddd139a7
上乃　聖　 <sei_ueno@sueno.local>
2016-03-03 14:07:54 +0900

    add files

---
+++ stop_program.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ "$(pidof node)" ]
+then
+  killall node
+fi
+
+if [ "$(pidof yarpserver)" ]
+then
+  killall yarpserver
+fi
+
+if [ "$(pidof python)" ]
+then
+  killall ipython
+fi
+
+if [ "$(pidof gzserver)" ]
+then
+  killall gzserver
+fi
+
+if [ "$(pidof Xvfb)" ]
+then
+  killall Xvfb
+fi
